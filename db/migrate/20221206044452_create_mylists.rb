b41aaafac54ed331aef2c50cb4304fd352a4d427
RenSaitou <112914054+DMMwcRen753@users.noreply.github.com>
2022-12-06 05:09:14 +0000

    migrate

---
+++ db/migrate/20221206044452_create_mylists.rb
@@ -0,0 +1,11 @@
+class CreateMylists < ActiveRecord::Migration[6.1]
+  def change
+    create_table :mylists do |t|
+      
+      t.integer :user_id, null: false, foreign_key: true
+      t.string :name,     null: 
+
+      t.timestamps
+    end
+  end
+end
